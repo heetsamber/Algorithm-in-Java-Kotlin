0ae83d1c64a28f6a3a95da7f2a4894116149d54c
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-01-19 23:35:33 +0900

    Create HeapSort.java

---
+++ Java/Sort/HeapSort/HeapSort.java
@@ -0,0 +1,61 @@
+public class HeapSort {
+    public void sort(int arr[])
+    {
+        int N = arr.length;
+      
+        for (int i = N / 2 - 1; i >= 0; i--)
+            heapify(arr, N, i);
+
+        for (int i = N - 1; i > 0; i--) {
+
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    void heapify(int arr[], int N, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+ 
+        // If left child is larger than root
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+ 
+        // If right child is larger than largest so far
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, N, largest);
+        }
+    }
+ 
+    static void printArray(int arr[])
+    {
+        int N = arr.length;
+ 
+        for (int i = 0; i < N; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        int N = arr.length;
+ 
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+ 
+        System.out.println("정렬 후: ");
+        printArray(arr);
+    }
+}
