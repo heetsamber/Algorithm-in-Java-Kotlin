2983af589d0d2a6ef7623f1a137122e2c704cc3e
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-01-22 04:51:39 +0900

    Update EuclideanAlgorithm.kt

--- Kotlin/Math/EuclideanAlgorithm.kt
+++ Kotlin/Math/EuclideanAlgorithm.kt
@@ -1,2 +1,13 @@
 // Time Complexity: O(Log min(a, b))
 
+fun gcd(a : Int, b: Int): Int{
+    if (a == 0) return b;
+
+    return gcd(b % a, a);
+}
+fun main(){
+    val a = 1450
+    val b = 450
+    val result = gcd(a, b)
+    println("gcd = " + result)
+}
