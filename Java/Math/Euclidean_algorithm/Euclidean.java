cabe550bd3e210f9c833a27fc9249094fa562ca8
bu <103236108+heetsamber@users.noreply.github.com>
2023-02-02 23:27:54 +0900

    euclidean

--- Java/Math/Euclidean_algorithm/Euclidean.java
+++ Java/Math/Euclidean_algorithm/Euclidean.java
@@ -1,1 +1,14 @@
+class Euclidean{
+    public static int gcd(int a, int b){
+        if(a == 0) return b;
 
+        return gcd(b % a, a);
+    }
+
+    public static void main(String[] args){
+        int a = 12, b = 36, result;
+
+        result = gcd(a, b);
+        System.out.println("gcd : " + result);
+    }
+}
