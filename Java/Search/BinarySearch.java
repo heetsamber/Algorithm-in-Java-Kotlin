d312cbfc357d1238581b8b33e1af6843d7c7b520
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-02-10 18:30:58 +0900

    Update BinarySearch.java

--- Java/Search/BinarySearch.java
+++ Java/Search/BinarySearch.java
@@ -7,7 +7,7 @@ public class BinarySearch {
             // 시작 지점은 0이기떄문에 오버플로우가 나지않음
             int mid = (start + end) / 2;
             if (x == arr[mid]) {
-                return mid;
+                return mid;     // 인덱스 반환
             }
             else if (x > arr[mid]) {
                 start = mid + 1;
@@ -22,7 +22,7 @@ public class BinarySearch {
     public static void main(String[] args)
     {
         int arr[] = { 2, 4, 5, 17, 14, 7, 11, 22 };
-        int search = binary(arr, 22);
+        int search = binary(arr, 22);           // 정렬되어 있지 않은 배열
         System.out.println(search);
     }
 }
