b7a5a911f66794952113bc984a8c186d3f59a289
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-03-16 10:04:40 +0900

    Update FloydWarshall.java

--- Java/Graph/FloydWarshall/FloydWarshall.java
+++ Java/Graph/FloydWarshall/FloydWarshall.java
@@ -6,17 +6,9 @@ class AllPairShortestPath {
 	final static int INF = 99999, V = 4;
 
 	void floydWarshall(int dist[][]){
-		
-		int i, j, k;
-		for (k = 0; k < V; k++) {
-			// Pick all vertices as source one by one
-			for (i = 0; i < V; i++) {
-				// Pick all vertices as destination for the
-				// above picked source
-				for (j = 0; j < V; j++) {
-					// If vertex k is on the shortest path
-					// from i to j, then update the value of
-					// dist[i][j]
+		for (int k = 0; k < V; k++) {
+			for (int i = 0; i < V; i++) {
+				for (int j = 0; j < V; j++) {
 					if (dist[i][k] + dist[k][j]
 						< dist[i][j])
 						dist[i][j]
@@ -24,15 +16,11 @@ class AllPairShortestPath {
 				}
 			}
 		}
-
-		// Print the shortest distance matrix
 		printSolution(dist);
 	}
 
 	void printSolution(int dist[][]){
 		System.out.println(
-			"The following matrix shows the shortest "
-			+ "distances between every pair of vertices");
 		for (int i = 0; i < V; ++i) {
 			for (int j = 0; j < V; ++j) {
 				if (dist[i][j] == INF)
