40245d8fa67636f25728fc3e104fa9593f42362b
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-03-16 10:07:36 +0900

    Update FloydWarshall.java

--- Java/Graph/FloydWarshall/FloydWarshall.java
+++ Java/Graph/FloydWarshall/FloydWarshall.java
@@ -1,38 +1,45 @@
+package Graph.Floyd;
+
 import java.io.*;
 import java.lang.*;
 import java.util.*;
 
 class AllPairShortestPath {
-	final static int INF = 99999, V = 4;
+    final static int INF = 99999, V = 4;
 
-	void floydWarshall(int dist[][]){
-		for (int k = 0; k < V; k++) {
-			for (int i = 0; i < V; i++) {
-				for (int j = 0; j < V; j++) {
-					if (dist[i][k] + dist[k][j]
-						< dist[i][j])
-						dist[i][j]
-							= dist[i][k] + dist[k][j];
-				}
-			}
-		}
-		printSolution(dist);
-	}
+    void floydWarshall(int dist[][]) {
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (dist[i][k] + dist[k][j]
+                            < dist[i][j])
+                        dist[i][j]
+                                = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+        printSolution(dist);
+    }
 
-	void printSolution(int dist[][]){
-		System.out.println(
-		for (int i = 0; i < V; ++i) {
-			for (int j = 0; j < V; ++j) {
-				if (dist[i][j] == INF)
-					System.out.print("INF ");
-				else
-					System.out.print(dist[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
+    void printSolution(int dist[][])
+    {
+        System.out.println(
+                "The following matrix shows the shortest "
+                        + "distances between every pair of vertices");
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
+                if (dist[i][j] == INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
-	public static void main(String[] args){
+    // Driver's code
+    public static void main(String[] args)
+    {
 		/* Let us create the following weighted graph
 		10
 		(0)------->(3)
@@ -42,12 +49,12 @@ class AllPairShortestPath {
 		\|/		 |
 		(1)------->(2)
 		3		 */
-		int graph[][] = { { 0, 5, INF, 10 },
-						{ INF, 0, 3, INF },
-						{ INF, INF, 0, 1 },
-						{ INF, INF, INF, 0 } };
-		AllPairShortestPath a = new AllPairShortestPath();
-		a.floydWarshall(graph);
-	}
+        int graph[][] = { { 0, 5, INF, 10 },
+                { INF, 0, 3, INF },
+                { INF, INF, 0, 1 },
+                { INF, INF, INF, 0 } };
+        AllPairShortestPath a = new AllPairShortestPath();
+        a.floydWarshall(graph);
+    }
 }
 
